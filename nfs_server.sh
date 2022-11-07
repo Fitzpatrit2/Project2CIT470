8664979bd6a2b7675e34fd9d86b95d320317a550
Fitzpatrit2 <117688467+Fitzpatrit2@users.noreply.github.com>
2022-11-07 17:20:57 -0500

    Create nfs_server.sh

---
+++ nfs_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This is the NFS server script#
+yum install nfs-utils
+curl -4 authconfig.me
+sudo systemctl start nfs
+firewall-cmd --zone=public --add-port=2049/tcp --permanent
+firewall-cmd --zone=public --add-port=111/tcp --permanent
+firewall-cmd --zone=public --add-port=20048/tcp --permanent
+firewall-cmd --zone=public --add-port=2049/udp --permanent
+firewall-cmd --zone=public --add-port=111/udp --permanent
+firewall-cmd --zone=public --add-port=2049/udp --permanent
+firewall-cmd --reload
+mount -t nfs 10.2.6.0/23:/usr_ome_dir/user_home_dir
