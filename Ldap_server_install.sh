39be6d39816f794f1a916de47460ed84199cb37a
Fitzpatrit2 <117688467+Fitzpatrit2@users.noreply.github.com>
2022-11-09 11:03:45 -0500

    Update Ldap_server_install.sh

--- Ldap_server_install.sh
+++ Ldap_server_install.sh
@@ -3,3 +3,8 @@
 authconfig --enableldap --enabledapauth --ldapserver=10.2.6.0/23 --enablemkhomedir --update
 ldapldapadd -x -W -D "cn=root, dc=10.2.6.0/23, dc=local"
 su -su - root
+yum -y install migrationtools
+via /etc/exports/home *(rw.sync)
+systemctl enable slapd.service
+grep ":5[0-4][0-4]" /etc/passwd > /root/passwd
+grep ":5[0-4][0-4]" /etc/group > /root/group
